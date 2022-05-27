632b50ad9ef29de92a01b1cc22369925f9346c92
Oguz <oguzmutlu81@gmail.com>
2022-05-27 20:51:17 +0300

    toplanan verilerin cizgi grafiklerinin eklenmesi
    Figurler klasoru olusturulup toplanan verilerin grafiklestirilmesi icin butun test datalarinin cizgi grafigi olusturuldu.

---
+++ graphics.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+columns = ["Sensor4","Sensor3","Sensor2","Sensor1"]
+file = pd.read_csv("data/device_data/u_24/v_7.csv", names=columns)
+
+fig, ax = plt.subplots()
+fig.set_size_inches(13, 8, forward=True)
+# x = range(file.Sensor4.size)
+x = np.arange(0, file.Sensor4.size, 1)
+y = []
+
+y = file.Sensor1;
+
+ax.plot(x,y,marker='o', markersize=3, label='29mm');
+ax.plot(x,file.Sensor3,marker='o', markersize=3,label='24mm')
+ax.plot(x, file.Sensor2,marker='o', markersize=3,label='10mm')
+ax.plot(x, file.Sensor4,marker='o', markersize=3,label='5mm')
+ax.legend(loc='upper right',prop={'size': 6})
+
+plt.savefig('data/figures/u_24/line/v7.png')
